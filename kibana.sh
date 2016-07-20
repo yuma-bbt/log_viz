3b93b297040d63f342de9bd6efb81b23959c5822
iwamoto yuma <yuma@yumac.local>
2016-07-20 15:12:09 +0900

    add source code

---
+++ kibana.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#tdagent
+curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-trusty-td-agent2.sh | sh
+/etc/init.d/td-agent restart
+/etc/init.d/td-agent status
+#!/bin/sh
+#tdagent
+curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-trusty-td-agent2.sh | sh
+/etc/init.d/td-agent restart
+/etc/init.d/td-agent status
+
+echo "
+<source>
+ type syslog
+ port 5140
+ tag  syslog
+</source>
+
+<source>
+ type forward
+</source>
+
+<match syslog.*.*>
+ type elasticsearch
+ logstash_format true
+ flush_interval 10s
+</match>
+" > /etc/td-agent.conf
+
+
+#java
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get -y install oracle-java8-installer
+#elasticsearch 
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install elasticsearch -y 
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
+sudo /etc/init.d/elasticsearch status
+#kibana 
+wget https://download.elastic.co/kibana/kibana/kibana-4.5.1-linux-x64.tar.gz
+tar xfvz kibana-4.5.1-linux-x64.tar.gz
+./kibana-4.5.1-linux-x64/bin/kibana&
+sleep 10 
+curl -XGET http://localhost:9200
+
